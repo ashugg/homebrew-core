3acb794b630e75d6d762c82c3a007da365463e3b
botantony <antonsm21@gmail.com>
2026-06-06 13:59:39 +0200

    opencv 5.0.0
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/o/opencv.rb
+++ Formula/o/opencv.rb
@@ -2,16 +2,15 @@ class Opencv < Formula
   desc "Open source computer vision library"
   homepage "https://opencv.org/"
   license "Apache-2.0"
-  revision 12
   compatibility_version 1
 
   stable do
-    url "https://github.com/opencv/opencv/archive/refs/tags/4.13.0.tar.gz"
-    sha256 "1d40ca017ea51c533cf9fd5cbde5b5fe7ae248291ddf2af99d4c17cf8e13017d"
+    url "https://github.com/opencv/opencv/archive/refs/tags/5.0.0.tar.gz"
+    sha256 "b0528f5a1d379d59d4701cb28c36e22214cc51cf64594e5b56f2d3e6c0233095"
 
     resource "contrib" do
-      url "https://github.com/opencv/opencv_contrib/archive/refs/tags/4.13.0.tar.gz"
-      sha256 "1e0077a4fd2960a7d2f4c9e49d6ba7bb891cac2d1be36d7e8e47aa97a9d1039b"
+      url "https://github.com/opencv/opencv_contrib/archive/refs/tags/5.0.0.tar.gz"
+      sha256 "c58f6344170c39abf187c56f3843b59cab1fd3e89cf19ba2ce25dc061659b27f"
 
       livecheck do
         formula :parent
@@ -93,13 +92,8 @@ class Opencv < Formula
     ENV["OpenBLAS_HOME"] = Formula["openblas"].opt_prefix
 
     # Remove bundled libraries to make sure formula dependencies are used
-    libdirs = %w[ffmpeg libjasper libjpeg libjpeg-turbo libpng libtiff libwebp openexr openjpeg protobuf tbb zlib]
+    libdirs = %w[ffmpeg libjasper libjpeg-turbo libpng libtiff libwebp openjpeg protobuf tbb zlib]
     libdirs.each { |l| rm_r(buildpath/"3rdparty"/l) }
-
-    # Fix OpenVINO 2026 Tensor::data() constness mismatch, upstream bug report, https://github.com/opencv/opencv/issues/28586
-    inreplace "modules/dnn/src/op_inf_engine.cpp",
-              "return Mat(size, type, blob.data());",
-              "return Mat(size, type, const_cast<void*>(blob.data()));"
 
     # VTK 9.6 stopped transitively including <iostream>;
     # viz uses std::cout/endl directly.
